ae13efafae231f50082228b35c300d11ef5eac44
Collin <collinhallett@gmail.com>
2021-07-03 15:38:25 -0500

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,15 @@
+function fight() {
+    window.alert("The fight has begun!");
+
+}
+
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+var name = "your name";
+console.log(name);
+
+//fight();
+
+
+
